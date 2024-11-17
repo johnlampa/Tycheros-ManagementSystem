f161f11de3b7a4b8988e64d52a2b2f25e7802749
John Kyle Lampa <jklampa31@gmail.com>
2024-11-17 13:16:42 +0800

    same implementation as status records

--- FrontEnd/src/components/PriceRecordsModal.tsx
+++ FrontEnd/src/components/PriceRecordsModal.tsx
@@ -1,5 +1,6 @@
 import Modal from "@/components/ui/Modal";
 import { useEffect, useState } from "react";
+import { Employee } from "../../lib/types/EmployeeDataTypes";
 
 interface PriceRecordsModal {
   productID: number | undefined;
@@ -54,7 +55,30 @@ const PriceRecordsModal: React.FC<PriceRecordsModal> = ({
     };
 
     fetchPriceRecords();
-}, [productID]);
+  }, [productID]);
+
+  const [employees, setEmployees] = useState<Employee[]>([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8081/employeeManagement/getEmployee"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data: Employee[] = await response.json();
+        setEmployees(data);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
 
   return (
     <Modal
@@ -62,35 +86,42 @@ const PriceRecordsModal: React.FC<PriceRecordsModal> = ({
       setModalVisibility={setPriceRecordsModalIsVisible}
     >
       <div className="w-[340px] p-6 mx-auto rounded">
-        <div className="text-center text-xl font-bold text-black mb-4">
+        <div className="text-center text-xl font-bold text-black mb-4 ">
           Price Records
         </div>
-        <div className="grid grid-cols-[1fr_1fr_1fr] font-semibold text-sm">
+        <div className="grid grid-cols-[1fr_1fr_1fr] font-semibold text-sm gap-x-1">
           <div className="flex justify-center items-center">Price</div>
-          <div className="flex justify-center items-center text-center ">
+          <div className="flex justify-center items-center text-center">
             Date Changed
           </div>
-          <div className="flex justify-center items-center">Employee ID</div>
+          <div className="flex justify-center items-center">Employee</div>
         </div>
-        {priceRecordsForProduct.map((priceRecord, priceRecordIndex) => (
-          <div
-            key={priceRecordIndex}
-            className="grid grid-cols-[1fr_1fr_1fr] mt-2 text-sm gap-y-1"
-          >
-            <div className="flex justify-center items-center">
-              &#8369; {priceRecord.sellingPrice}
+        {priceRecordsForProduct.map((priceRecord, priceRecordIndex) => {
+          const employee = employees.find(
+            (emp) => emp.employeeID === priceRecord.employeeID
+          );
+          return (
+            <div
+              key={priceRecordIndex}
+              className="grid grid-cols-[1fr_1fr_1fr] mt-2 text-sm gap-y-1 gap-x-1"
+            >
+              <div className="flex justify-center items-center">
+                &#8369; {priceRecord.sellingPrice}
+              </div>
+              <div className="flex justify-center items-center text-center">
+                {priceRecord.priceDateTime.substring(0, 10)}
+              </div>
+              <div className="flex justify-center items-center text-xs text-center">
+                {employee
+                  ? `${employee.lastName}, ${employee.firstName}`
+                  : "Unknown"}
+              </div>
             </div>
-            <div className="flex justify-center items-center">
-              {priceRecord.priceDateTime.substring(0, 10)}
-            </div>
-            <div className="flex justify-center items-center">
-              {priceRecord.employeeID}
-            </div>
-          </div>
-        ))}
+          );
+        })}
         <div className="mt-8 text-center flex justify-end">
           <button
-            className=" w-min bg-white hover:bg-gray hover:text-white text-gray border-2 border-gray font-semibold py-2 px-4 rounded"
+            className="w-min bg-white hover:bg-gray hover:text-white text-gray border-2 border-gray font-semibold py-2 px-4 rounded"
             onClick={(e) => {
               e.preventDefault();
               setPriceRecordsModalIsVisible(false);
