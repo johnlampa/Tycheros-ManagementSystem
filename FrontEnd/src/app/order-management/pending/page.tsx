784bff6bb5eff23e2a52f9249923d559f875e779
John Kyle Lampa <jklampa31@gmail.com>
2024-11-21 08:18:26 +0800

    added loggedinemployeeid

--- FrontEnd/src/app/order-management/pending/page.tsx
+++ FrontEnd/src/app/order-management/pending/page.tsx
@@ -30,6 +30,16 @@ export default function Page() {
     number | undefined
   >(0);
 
+  const [loggedInEmployeeID, setLoggedInEmployeeID] = useState(-1);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const loggedInEmployeeID = localStorage.getItem("loggedInEmployeeID");
+      if (loggedInEmployeeID) {
+        setLoggedInEmployeeID(parseInt(loggedInEmployeeID));
+      }
+    }
+  }, []);
+
   useEffect(() => {
     const fetchOrders = async () => {
       try {
@@ -150,6 +160,7 @@ export default function Page() {
           orderToEdit={orderToEdit}
           orders={orders}
           setOrders={setOrders}
+          loggedInEmployeeID={loggedInEmployeeID}
         ></CancelOrderModal>
 
         <StatusRecordsModal
